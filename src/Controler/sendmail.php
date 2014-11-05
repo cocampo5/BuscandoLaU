20b5cee6b1f7135f8fc420abe8bc08ed19c142d3
Andrés Mateo Otálvaro <mateito1135@hotmail.com>
2014-11-04 23:19:57 -0500

    .....

--- src/Controler/sendmail.php
+++ src/Controler/sendmail.php
@@ -4,32 +4,26 @@
   
   class sendmail{
 
-    var $mail;
-
       function sendmail(){
           
-          $this->$mail = new phpmailer();
-          $this->$mail->PluginDir = "src/Controler/phpmailer/";
-          $this->$mail->Mailer = "smtp";
-          $this->$mail->Port = "2525";
-         $this->$mail->Host = "mx1.hostinger.co";
-          $this->$mail->SMTPAuth = true;
-          $this->$mail->Username = "webmaster@buscandolau.wc.lt"; 
-          $this->$mail->Password = "eafit2014";
-          $this->$mail->From = "webmaster@buscandolau.wc.lt";
-          $this->$mail->FromName = "BuscandoLaU - Admin";
-          $this->$mail->Timeout=30;
-          $this->$mail->AddAddress("andressmateo@hotmail.com");
-          $this->$mail->Subject = "Contacto";
-          $this->$mail->Body = "<b>BuscandoLaU</b>
+          $mail = new phpmailer();
+          $mail->PluginDir = "src/Controler/phpmailer/";
+          $mail->Mailer = "smtp";
+          $mail->Port = "2525";
+          $mail->Host = "mx1.hostinger.co";
+          $mail->SMTPAuth = true;
+          $mail->Username = "webmaster@buscandolau.wc.lt"; 
+          $mail->Password = "eafit2014";
+          $mail->From = "webmaster@buscandolau.wc.lt";
+          $mail->FromName = "BuscandoLaU - Admin";
+          $mail->Timeout=30;
+          $mail->AddAddress("andressmateo@hotmail.com");
+          $mail->Subject = "Contacto";
+          $mail->Body = "<b>BuscandoLaU</b>
                             <br>
                             Muchas gracias por contactarnos, recibirás respuesta en un plazo máximo de 24 horas
                         ";
-         
-      }
-
-      function enviar(){
-        $mail->AltBody = "Muchas gracias por contactarnos, recibirás respuesta en un plazo máximo de 24 horas";
+          $mail->AltBody = "Muchas gracias por contactarnos, recibirás respuesta en un plazo máximo de 24 horas";
           $exito = $mail->Send();
           $intentos=1; 
           while ((!$exito) && ($intentos < 6)) {
@@ -48,12 +42,9 @@
             echo "Mensaje enviado correctamente";
             //header("Location:../index.html");
           } 
-
-        $sender = new sendmail();
-        $sender.enviar();
       }
   }
 
-  //
+  //$sender = new sendmail();
   
 ?>
