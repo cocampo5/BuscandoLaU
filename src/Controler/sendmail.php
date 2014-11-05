ab3dbf85050fdda5e72e8c3c9fefe0ea08d1d9ce
cocampo5 <cocampo503@gmail.com>
2014-11-04 21:42:49 -0500

    Modificando cositas, se agregaron poquitos pregrados al test

--- src/Controler/sendmail.php
+++ src/Controler/sendmail.php
@@ -1,82 +1,49 @@
 <?php
-  // primero hay que incluir la clase phpmailer para poder instanciar
-  //un objeto de la misma
-  require "src/Controler/phpmailer/class.phpmailer.php";
-  require 'src/Controler/phpmailer/PHPMailerAutoload.php';
-  //instanciamos un objeto de la clase phpmailer al que llamamos 
-  //por ejemplo mail
-  $mail = new phpmailer();
+  require "phpmailer/class.phpmailer.php";
+  require 'phpmailer/PHPMailerAutoload.php';
+  
+  class sendmail{
 
-  //Definimos las propiedades y llamamos a los métodos 
-  //correspondientes del objeto mail
+      function sendmail(){
+          
+          $mail = new phpmailer();
+          $mail->PluginDir = "src/Controler/phpmailer/";
+          $mail->Mailer = "smtp";
+          $mail->Port = "2525";
+          $mail->Host = "mx1.hostinger.co";
+          $mail->SMTPAuth = true;
+          $mail->Username = "webmaster@buscandolau.wc.lt"; 
+          $mail->Password = "eafit2014";
+          $mail->From = "webmaster@buscandolau.wc.lt";
+          $mail->FromName = "BuscandoLaU - Admin";
+          $mail->Timeout=30;
+          $mail->AddAddress("cocampo5@eafit.edu.co");
+          $mail->Subject = "Contacto";
+          $mail->Body = "<b>Titulo Correo</b>
+                            <br>
+                            Muchas gracias por contactarnos, recibiras respuesta dentro de 24 horas
+                        ";
+          $mail->AltBody = "Titulo correo. Muchas gracias por contactarnos, recibiras respuesta dentro de 24 horas";
+          $exito = $mail->Send();
+          $intentos=1; 
+          while ((!$exito) && ($intentos < 6)) {
+            sleep(5);
+            //echo $mail->ErrorInfo;
+            $exito = $mail->Send();
+            $intentos=$intentos+1;  
+          }
+          if(!$exito)
+          {
+            echo "Problemas enviando correo electronico a ".$valor;
+            echo "<br/>".$mail->ErrorInfo;  
+          }
+          else
+          {
+            echo "Mensaje enviado correctamente";
+          } 
+      }
+  }
 
-  //Con PluginDir le indicamos a la clase phpmailer donde se 
-  //encuentra la clase smtp que como he comentado al principio de 
-  //este ejemplo va a estar en el subdirectorio includes
-  $mail->PluginDir = "src/Controler/phpmailer/";
-
-  //Con la propiedad Mailer le indicamos que vamos a usar un 
-  //servidor smtp
-  $mail->Mailer = "smtp";
-
-  $mail->Port = "2525";
-
-  //Asignamos a Host el nombre de nuestro servidor smtp
-  $mail->Host = "mx1.hostinger.co";
-
-  //Le indicamos que el servidor smtp requiere autenticación
-  $mail->SMTPAuth = true;
-
-  //Le decimos cual es nuestro nombre de usuario y password
-  $mail->Username = "webmaster@buscandolau.wc.lt"; 
-  $mail->Password = "eafit2014";
-
-  //Indicamos cual es nuestra dirección de correo y el nombre que 
-  //queremos que vea el usuario que lee nuestro correo
-  $mail->From = "webmaster@buscandolau.wc.lt";
-  $mail->FromName = "webmaster";
-
-  //el valor por defecto 10 de Timeout es un poco escaso dado que voy a usar 
-  //una cuenta gratuita, por tanto lo pongo a 30  
-  $mail->Timeout=30;
-
-  //Indicamos cual es la dirección de destino del correo
-  $mail->AddAddress("andressmateo@hotmail.com");
-
-  //Asignamos asunto y cuerpo del mensaje
-  //El cuerpo del mensaje lo ponemos en formato html, haciendo 
-  //que se vea en negrita
-  $mail->Subject = "Teo es puto";
-  $mail->Body = "<b>Teo es puto</b>";
-
-  //Definimos AltBody por si el destinatario del correo no admite email con formato html 
-  $mail->AltBody = "Hola Daniel";
-
-  //se envia el mensaje, si no ha habido problemas 
-  //la variable $exito tendra el valor true
-  $exito = $mail->Send();
-
-  //Si el mensaje no ha podido ser enviado se realizaran 4 intentos mas como mucho 
-  //para intentar enviar el mensaje, cada intento se hara 5 segundos despues 
-  //del anterior, para ello se usa la funcion sleep	
-  $intentos=1; 
-  while ((!$exito) && ($intentos < 6)) {
-	sleep(5);
-     	//echo $mail->ErrorInfo;
-     	$exito = $mail->Send();
-     	$intentos=$intentos+1;	
-	
-   }
- 
-		
-   if(!$exito)
-   {
-	echo "Problemas enviando correo electronico a ".$valor;
-	echo "<br/>".$mail->ErrorInfo;	
- 
-   }
-   else
-   {
-	echo "Mensaje enviado correctamente";
-   } 
+  $sender = new sendmail();
+  
 ?>
