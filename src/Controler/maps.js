e6383a8c5d14a2d896cb1f11aa6122e22384a498
cocampo5 <cocampo503@gmail.com>
2014-11-20 18:13:24 -0500

    Trabajando con los mapas, se puede crear marker con boton

--- src/Controler/maps.js
+++ src/Controler/maps.js
@@ -1,5 +1,6 @@
+var pos;
+var map;
 var u1=new google.maps.LatLng(6.20122,-75.57843); //Coordenadas de EAFIT
-
 function initialize()
 {
   var mapProp = {
@@ -8,16 +9,19 @@ function initialize()
     mapTypeId:google.maps.MapTypeId.ROADMAP
   };
 
-  var map=new google.maps.Map(document.getElementById("googleMap")
+  map=new google.maps.Map(document.getElementById("googleMap")
     ,mapProp);
 
 
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = new google.maps.LatLng(position.coords.latitude,
+      pos = new google.maps.LatLng(position.coords.latitude,
        position.coords.longitude);
-      alert(pos);
+      //alert(pos);
       
+      var show = google.maps.geometry.spherical.computeDistanceBetween(pos,u1);
+      alert(show);
+
       var h = new google.maps.Marker({
         position:pos,
       });
@@ -40,27 +44,71 @@ function initialize()
     position:u1,
   });
 
+
   uni1.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
   uni1.setMap(map);
+
+
+
 }
 
-function handleNoGeolocation(errorFlag) {
-  if (errorFlag) {
-    var content = 'Error: The Geolocation service failed.';
-  } else {
-    var content = 'Error: Your browser doesn\'t support geolocation.';
+function markers(){
+    
+    var u2=new google.maps.LatLng(6.267186,-75.569153);
+    var u3=new google.maps.LatLng(6.20122,-75.57843);
+    var u4=new google.maps.LatLng(6.24204,-75.5895);
+    var u5=new google.maps.LatLng(6.2295797,-75.6079949);
+    var u6=new google.maps.LatLng(6.244711,-75.550232);
+
+    var uni2=new google.maps.Marker({
+      position:u2,
+    });
+
+    var uni3=new google.maps.Marker({
+      position:u3,
+    });
+
+    var uni4=new google.maps.Marker({
+      position:u4,
+    });
+
+    var uni5=new google.maps.Marker({
+      position:u5,
+    });
+
+    var uni6=new google.maps.Marker({
+      position:u6,
+    });
+
+    var markers = [];
+
+    //markers.push(u1);
+    markers.push(u2);
+    markers.push(u3);
+    markers.push(u4);
+    markers.push(u5);
+    markers.push(u6);
+    alert(markers);
+     uni6.setMap(map);
   }
 
-  var options = {
-    map: map,
-    position: new google.maps.LatLng(0, 0),
-    content: content
-  };
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag) {
+      var content = 'Error: The Geolocation service failed.';
+    } else {
+      var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
 
-  var infowindow = new google.maps.InfoWindow(options);
-  map.setCenter(options.position);
-}
+    var options = {
+      map: map,
+      position: new google.maps.LatLng(0, 0),
+      content: content
+    };
+
+    var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
+  }
 
 
 
-google.maps.event.addDomListener(window, 'load', initialize);
+  google.maps.event.addDomListener(window, 'load', initialize);
